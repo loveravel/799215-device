04a370e69aebf7f98ea764831c702c70611580e6
loveravel <42149116+loveravel@users.noreply.github.com>
2018-09-10 03:31:59 +0400

    Подключение скриптов

---
+++ js/modal-write-us-show.js
@@ -0,0 +1,18 @@
+  var link = document.querySelector(".btn-write-us");
+  
+  var popup = document.querySelector(".modal-login");
+  var close = popup.querySelector(".modal-close");
+  
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+  });
+
+var btn_write_us = document.querySelector(".btn-write-us");
+var modal_write_us = document.querySelector(".modal-write-us");
+var close_write_us = modal_write_us.querySelector(".modal-close");
+
+write_us.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    
+});
